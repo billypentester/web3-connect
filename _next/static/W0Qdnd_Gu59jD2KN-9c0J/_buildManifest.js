a9c37766f9b4877134a823f2a7cb41814cc3248b
Bilal Ahmad <BilalSheikh2500@gmail.com>
2022-05-02 13:00:35 +0500

    add files

---
+++ _next/static/W0Qdnd_Gu59jD2KN-9c0J/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/347-aed9ca184fbaa37c.js","static/chunks/pages/index-71a1a2033ecd0d05.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
